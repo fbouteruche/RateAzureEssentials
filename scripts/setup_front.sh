3da4830b9170d8f7d1dfd495cd0732648783bcde
François Bouteruche <frbouter@microsoft.com>
2018-09-19 10:48:27 +0200

    Fix setup script

--- scripts/setup_front.sh
+++ scripts/setup_front.sh
@@ -4,9 +4,11 @@ apt-get update -y
 apt-get upgrade -y 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs 
+sudo apt-get install -y npm
+npm install -g typescript
 mkdir /app
 cd /app
 git clone https://github.com/fbouteruche/RateAzureEssentials.git
 cd RateAzureEssentials/web
 npm install
-npm run server & echo "web server started"
+npm run serve & echo "web server started"
