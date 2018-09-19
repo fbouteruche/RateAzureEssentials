eeb4d694f9184a699682d5f290004861d4609725
François Bouteruche <frbouter@microsoft.com>
2018-09-19 11:04:43 +0200

    Add setup script for the api machines

---
+++ scripts/setup_middle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Same for Front and middle
+apt-get update -y
+apt-get upgrade -y 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs 
+sudo apt-get install -y npm
+npm install -g typescript
+mkdir /app
+cd /app
+git clone https://github.com/fbouteruche/RateAzureEssentials.git
+cd RateAzureEssentials/api
+npm run serve & echo "api server started"
