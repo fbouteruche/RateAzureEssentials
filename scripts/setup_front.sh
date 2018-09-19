0a8fddfff814db64a510b5ab8578a38624a1c7a1
François Bouteruche <frbouter@microsoft.com>
2018-09-19 10:37:13 +0200

    call npm install and then start the server

--- scripts/setup_front.sh
+++ scripts/setup_front.sh
@@ -4,4 +4,9 @@ apt-get update -y
 apt-get upgrade -y 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs 
+mkdir /app
+cd /app
 git clone https://github.com/fbouteruche/RateAzureEssentials.git
+cd RateAzureEssentials/web
+npm install
+npm run server & echo "web server started"
